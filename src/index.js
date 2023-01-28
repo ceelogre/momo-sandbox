22c16954bc1700ac4a72a969628fb54e54f063e3
Samuel Nishimwe <ceelogre@outlook.com>
2023-01-28 18:09:07 +0200

    ch: deploy (#1)
    * ch: deploy
    
    * feat: add dotenv
    
    * ch: add to gitignore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import   express  from "express";
 const app = express()
+import * as dotenv from "dotenv"
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Momo api'
   })
 })
-
-app.listen(3003, () => 'Listening...')
+const port = process.env.NODE_ENV === 'production'? process.env.PORT: 3003
+app.listen(port, () => {'Listening...'})
